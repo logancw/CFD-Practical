563e5760b97c2382ffd144da2a781101fe622862
Logan Weisgerber <logan.weisgerber@rcsd.ca>
2024-01-29 10:34:06 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Write a Bowling program that will allow the user to enter any number of bowling scores (integers between 0 and 300).  The user will enter a -1 to signal that they are done entering scores.  The application will then return the percentage of bowlers who are elite (Scores 200+) and average bowlers (less than 200). 
+
+
+
+You output should look similar to below:
+
+
+
+Please enter the bowling score (-1 to quit):189
+Please enter the bowling score (-1 to quit):193
+Please enter the bowling score (-1 to quit):213
+Please enter the bowling score (-1 to quit):-1
+
+Elite Bowlers 33.3%
+Average Bowlers 66.7%
+
+HINT: Ask for the first grade and then start the loop. Ask for the next grade inside the loop at the end. 
+
---
+++ src/App.java
@@ -0,0 +1,60 @@
+
+
+// -1 to end 
+// 200+ is Expert
+// < 200 is avg 
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App { 
+    public static void main(String[] args) throws Exception {
+        Scanner kb = new Scanner(System.in);
+        boolean stopped = true; 
+        ArrayList<Integer> scores = new ArrayList<Integer>();
+
+        System.out.println("Please enter the bowling score (-1 to quit): ");
+        int submission = kb.nextInt();
+
+        if(submission == -1){
+            System.out.println("Submissions stopped");
+        }else{
+            scores.add(submission);
+            stopped = false;
+        }
+
+        while(stopped == false){
+            System.out.println("Please enter the bowling score (-1 to quit: ");
+            submission = kb.nextInt();
+            if(submission== -1){
+                System.out.println("Submissions stopped");
+                stopped = true;
+            }else{
+                scores.add(submission);
+                stopped = false;
+            }
+        }
+
+        System.out.println("The bowling scores are: " + scores);
+        
+        int totalScores = scores.size();
+        int elite = 0;
+        int average = 0;
+
+        for(int i = 0; i < scores.size(); i++){
+            if(scores.get(i) >= 200){
+                elite++;
+            }else{
+                average++;
+            }
+        }
+
+        double elitePercent = (double)elite / totalScores;
+        System.out.println(" Elite Bowlers %" +  (float) elitePercent * 100);
+
+        double averagePercent = (double)average / totalScores;
+        System.out.println(" Average Bowlers %" + (float) averagePercent * 100);
+
+
+    }
+}
